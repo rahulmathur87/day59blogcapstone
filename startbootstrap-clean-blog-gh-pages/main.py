6f55b808585b2e9232f0208faee4a2ec9817a10e
Rahul <rahulmathur87@gmail.com>
2024-08-06 02:57:56 +0530

    connected posts to api

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+blog_endpoint = "https://api.npoint.io/8ea399838b3778277af1"
+response = requests.get(blog_endpoint)
+response.raise_for_status()
+blog_data = response.json()
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', blog_data=blog_data)
 
 
 @app.route("/about")
